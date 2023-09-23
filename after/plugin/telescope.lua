8ab0bc6ecac3bda390cf2ce6dcffee146a98db30
Rebecca Clair <rebecca@heliasar.com>
2023-09-23 13:39:22 +0200

    feat: update telescope config set transparency and live grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,9 +1,18 @@
+-- require("telescope").setup({
+--   defaults = {
+--     winblend = 70
+--   }
+-- })
+
 local builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ss', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+-- vim.keymap.set('n', '<leader>ss', function()
+--   builtin.grep_string({ search = vim.fn.input("Grep > ") })
+-- end)
+vim.keymap.set('n', '<leader>ss', builtin.live_grep, {})
+
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
 -- See `:help telescope.builtin`
