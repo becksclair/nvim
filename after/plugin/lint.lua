78558c58f0c543dab2603fa5378d7c3cfa92546e
Rebecca Clair <rebecca@heliasar.com>
2023-09-23 13:35:47 +0200

    feat: database support

---
+++ after/plugin/lint.lua
@@ -0,0 +1,22 @@
+require('lint').linters_by_ft = {
+  markdown = {'vale',},
+  python = {'flake8', 'pylint',},
+  sql = {'sqlfluff',},
+
+}
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    require("lint").try_lint()
+  end,
+})
+
+-- local sqlfluff = require('lint').linters.sqlfluff
+-- sqlfluff.args = {
+--   'lint',
+--   '--dialect postgres',
+--   -- <- Add a new parameter here
+-- }
+
+vim.keymap.set("n", "<M-l>", require("lint").try_lint, { desc = "Lint file" })
+
