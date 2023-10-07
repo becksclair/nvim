0f5e96104201ce37d0fd70076901cbd9886441ce
Rebecca Clair <rebecca@heliasar.com>
2023-10-07 14:51:33 +0200

    chore: cleanup nvim-lint config

--- after/plugin/lint.lua
+++ after/plugin/lint.lua
@@ -1,7 +1,12 @@
 require('lint').linters_by_ft = {
   -- markdown = {'vale',},
-  python = {'flake8', 'pylint',},
-  sql = {'sqlfluff',},
+  python = { 'flake8', 'pylint', },
+  sql = { 'sqlfluff', },
+
+  bash = { 'shellcheck', },
+  shell = { 'shellcheck', },
+  zsh = { 'shellcheck', },
+  -- rust = { 'clippy' }
 }
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
@@ -10,12 +15,13 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   end,
 })
 
--- local sqlfluff = require('lint').linters.sqlfluff
--- sqlfluff.args = {
---   'lint',
---   '--dialect postgres',
---   -- <- Add a new parameter here
--- }
+require('lint').linters.shellcheck.args = {
+  '-s',
+  'bash',
+  '--format',
+  'json',
+  '-'
+}
 
-vim.keymap.set("n", "<M-l>", require("lint").try_lint, { desc = "Lint file" })
+vim.keymap.set("n", "<leader>bl", require("lint").try_lint, { desc = "Lint file" })
 
