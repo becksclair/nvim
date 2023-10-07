3406203a77c1bb2b00dbe8ef0375f4a575027d29
Rebecca Clair <rebecca@heliasar.com>
2023-10-07 14:48:59 +0200

    feat: more complete telescope config

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,24 @@
--- require("telescope").setup({
---   defaults = {
---     winblend = 70
---   }
--- })
+require("telescope").setup({
+  -- defaults = {
+  --   winblend = 70
+  -- }
+  pickers = {
+    colorscheme = {
+      enable_preview = true
+    }
+  },
+  file_ignore_patterns = { "node_modules", "target", ".git" },
+  -- mappings = {
+  --     n = {
+  --         ['<c-d>'] = require('telescope.actions').delete_buffer
+  --     }, -- n
+  --     i = {
+  --         ["<C-h>"] = "which_key",
+  --         ['<c-d>'] = require('telescope.actions').delete_buffer
+  --     } -- i
+  -- }
+})
+
 
 local builtin = require('telescope.builtin')
 
@@ -16,8 +32,11 @@ vim.keymap.set('n', '<leader>ss', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>sq', require('telescope.builtin').colorscheme, { desc = '[sq] Find color schemes' })
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles,
+  { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers,
+  { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -33,5 +52,9 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]resume' })
 vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
 
+
+-- vim.keymap.set('n', '<c-d>', require('telescope.actions').delete_buffer)
+-- vim.keymap.set('i', '<c-d>', require('telescope.actions').delete_buffer)
+--
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
