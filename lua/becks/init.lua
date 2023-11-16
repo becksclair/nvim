2a805c5b1f68e7ceaf6a3bcbcac7ac626b6dd159
Rebecca Clair <rebecca@heliasar.com>
2023-11-16 01:18:21 +0100

    chore: format

--- lua/becks/init.lua
+++ lua/becks/init.lua
@@ -10,24 +10,24 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
 function R(name)
-  require("plenary.reload").reload_module(name)
+    require("plenary.reload").reload_module(name)
 end
 
 autocmd('TextYankPost', {
-  group = yank_group,
-  pattern = '*',
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = 'IncSearch',
-      timeout = 40,
-    })
-  end,
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40
+        })
+    end
 })
 
-autocmd({ "BufWritePre" }, {
-  group = BecksGroup,
-  pattern = "*",
-  command = [[%s/\s\+$//e]],
+autocmd({"BufWritePre"}, {
+    group = BecksGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]]
 })
 
 vim.g.netrw_browse_split = 0
@@ -39,7 +39,7 @@ vim.g.netrw_winsize = 25
 -- vim.g.loaded_netrwPlugin = 1
 
 -- Configure blinking cursors
-vim.cmd[[
+vim.cmd [[
     :highlight Cursor gui=reverse guifg=NONE guibg=NONE
     :highlight Cursor gui=NONE guifg=bg guibg=fg
     set guicursor=i-ci-a-sm:block-Cursor/Cursor-blinkwait175-blinkoff150-blinkon150
