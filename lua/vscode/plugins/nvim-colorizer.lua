1e97a24b3de986289cd80a1e52bf495d1586e766
Rebecca Clair <rebecca@heliasar.com>
2024-02-02 05:47:47 +0100

    chore: delete vscode specifics

--- lua/vscode/plugins/nvim-colorizer.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'norcalli/nvim-colorizer.lua',
-  lazy = true,
-  event = "BufEnter",
-  config = function()
-    require('colorizer').setup()
-  end,
-}
--- lua/vscode/plugins/quick-lint-js.lua
+++
@@ -1,5 +0,0 @@
-return {
-  'quick-lint/quick-lint-js',
-  tag = '2.18.0',
-  lazy = true,
-}
