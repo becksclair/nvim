403ae8a70a25625496f7686c13e0c69d5b40b972
Rebecca Clair <rebecca@heliasar.com>
2024-07-08 02:04:38 +0200

    chore: updated gruber-darker colors

--- lua/becks/plugins/gruber-darker.lua
+++ lua/becks/plugins/gruber-darker.lua
@@ -1,7 +1,15 @@
 return {
-  "blazkowolf/gruber-darker.nvim",
-  lazy = true,
-  opts = {
-    bold = true,
+  {
+    "becksclair/gruber-darker.nvim",
+    branch = "feat/softer-colors",
+    lazy = true,
+    enabled = true,
+    priority = 1000,
+    opts = {
+      bold = true,
+      italic = {
+        strings = false,
+      },
+    }
   }
 }
