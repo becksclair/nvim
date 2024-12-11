26f21a1794feea52ee8a2b40fcd3bb703ba94ae7
Rebecca Clair <rebecca@heliasar.com>
2024-12-11 07:39:10 +0100

    feat: configure supermaven

--- lua/becks/plugins/supermaven.lua
+++ lua/becks/plugins/supermaven.lua
@@ -1,17 +1,88 @@
 return {
-  "supermaven-inc/supermaven-nvim",
-  enabled = false,
-  lazy = true,
-  event = "BufReadPost",
-  config = function()
-    require("supermaven-nvim").setup({
+  {
+    "supermaven-inc/supermaven-nvim",
+    enabled = true,
+    event = "VeryLazy",
+    opts = {
       keymaps = {
-        accept_suggestion = "<Tab>",
-        clear_suggestion = "<C-]>",
+        accept_suggestion = nil, -- handled by nvim-cmp / blink.cmp
       },
-      ignore_filetypes = {
-        env = false,
+      disable_inline_completion = vim.g.ai_cmp,
+    },
+  },
+
+  -- add ai_accept action
+  -- {
+  --   "supermaven-inc/supermaven-nvim",
+  --   opts = function()
+  --     require("supermaven-nvim.completion_preview").suggestion_group = "SupermavenSuggestion"
+  --     LazyVim.cmp.actions.ai_accept = function()
+  --       local suggestion = require("supermaven-nvim.completion_preview")
+  --       if suggestion.has_suggestion() then
+  --         LazyVim.create_undo()
+  --         vim.schedule(function()
+  --           suggestion.on_accept_suggestion()
+  --         end)
+  --         return true
+  --       end
+  --     end
+  --   end,
+  -- },
+
+  -- cmp integration
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   optional = true,
+  --   dependencies = { "supermaven-nvim" },
+  --   opts = function(_, opts)
+  --     if vim.g.ai_cmp then
+  --       table.insert(opts.sources, 1, {
+  --         name = "supermaven",
+  --         group_index = 1,
+  --         priority = 100,
+  --       })
+  --     end
+  --   end,
+  -- },
+
+  vim.g.ai_cmp and {
+    "saghen/blink.cmp",
+    optional = false,
+    dependencies = { "supermaven-nvim", "saghen/blink.compat" },
+    opts = {
+      sources = {
+        compat = { "supermaven" },
+        providers = { supermaven = { kind = "Supermaven" } },
       },
-    })
-  end,
+    },
+  } or nil,
+
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   optional = true,
+  --   event = "VeryLazy",
+  --   opts = function(_, opts)
+  --     table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("supermaven"))
+  --   end,
+  -- },
+
+  {
+    "folke/noice.nvim",
+    optional = true,
+    opts = function(_, opts)
+      vim.list_extend(opts.routes, {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "Starting Supermaven" },
+              { find = "Supermaven Free Tier" },
+              { find = "Supermaven Pro" },
+            },
+          },
+          skip = true,
+        },
+      })
+    end,
+  },
 }
