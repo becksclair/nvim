db3c22dcc365759263203393cf3067f7434ac45d
Rebecca Clair <rebecca@heliasar.com>
2024-12-11 07:39:53 +0100

    chore: update lazygit config

--- lua/becks/plugins/lazygit.lua
+++ lua/becks/plugins/lazygit.lua
@@ -11,10 +11,8 @@ return {
     "nvim-telescope/telescope.nvim",
     "nvim-lua/plenary.nvim",
   },
-  -- init = function ()
-  -- end,
   config = function()
-    vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
+    vim.g.lazygit_floating_window_winblend = 20 -- transparency of floating window
     vim.g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
     vim.g.lazygit_floating_window_border_chars = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' } -- customize lazygit popup window border characters
     vim.g.lazygit_floating_window_use_plenary = 1 -- use plenary.nvim to manage floating window if available
