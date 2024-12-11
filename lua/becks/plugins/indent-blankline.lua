7af24f8a0f4415d376928c1a60dedee3e0cf837b
Rebecca Clair <rebecca@heliasar.com>
2024-12-11 07:38:17 +0100

    chore: replaced indent-blankline with mini scope

--- lua/becks/plugins/indent-blankline.lua
+++ lua/becks/plugins/indent-blankline.lua
@@ -1,13 +1,60 @@
 return {
   -- Add indentation guides even on blank lines
   'lukas-reineke/indent-blankline.nvim',
-  lazy = true,
-  event = "BufRead",
+  -- lazy = true,
+  event = "BufEnter",
   enabled = false,
   -- Enable `lukas-reineke/indent-blankline.nvim`
   -- See `:help indent_blankline.txt`
-  opts = {
-    char = '┊',
-    show_trailing_blankline_indent = true,
-  }
+  opts = function()
+    Snacks.toggle({
+      name = "Indention Guides",
+      get = function()
+        return require("ibl.config").get_config(0).enabled
+      end,
+      set = function(state)
+        require("ibl").setup_buffer(0, { enabled = state })
+      end,
+    }):map("<leader>ug")
+
+    ---@type ibl.config.full
+    return {
+      debounce = 200,
+      indent = {
+        char = "│",
+        tab_char = nil,
+        -- tab_char = "│",
+        highlight = "IblIndent",
+        smart_indent_cap = true,
+      },
+      scope = {
+        show_start = true,
+        show_end = true,
+        show_exact_scope = true,
+      },
+      whitespace = {
+        highlight = "IblWhitespace",
+        remove_blankline_trail = true,
+      },
+      exclude = {
+        filetypes = {
+          "Trouble",
+          "alpha",
+          "dashboard",
+          "help",
+          "lazy",
+          "mason",
+          "neo-tree",
+          "notify",
+          "snacks_dashboard",
+          "snacks_notif",
+          "snacks_terminal",
+          "snacks_win",
+          "toggleterm",
+          "trouble",
+        },
+      },
+    }
+  end,
+  main = "ibl",
 }
