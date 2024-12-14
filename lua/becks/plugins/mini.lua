bfa51915ccba99bf60876b47d52368017d3997f0
Rebecca Clair <rebecca@heliasar.com>
2024-12-14 21:16:42 +0100

    feat: fix mini-surround setup

--- lua/becks/plugins/mini.lua
+++ lua/becks/plugins/mini.lua
@@ -8,7 +8,7 @@ return {
   config = function()
     require('mini.ai').setup({})
     require('mini.icons').setup {
-      style              = 'glyph',
+      style = 'glyph',
 
       -- -- Customize per category. See `:h MiniIcons.config` for details.
       -- default            = {},
@@ -43,5 +43,47 @@ return {
     }
 
     require('mini.diff').setup {}
+
+    require('mini.surround').setup {
+      -- Add custom surroundings to be used on top of builtin ones. For more
+      -- information with examples, see `:h MiniSurround.config`.
+      custom_surroundings = nil,
+
+      -- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
+      highlight_duration = 500,
+
+      -- Module mappings. Use `''` (empty string) to disable one.
+      mappings = {
+        add = 'gsa',          -- Add surrounding in Normal and Visual modes
+        delete = 'gsd',       -- Delete surrounding
+        find = 'gsf',         -- Find surrounding (to the right)
+        find_left = 'gsF',    -- Find surrounding (to the left)
+        highlight = 'gsh',    -- Highlight surrounding
+        replace = 'gsr',      -- Replace surrounding
+        update_n_lines = 'gsn', -- Update `n_lines`
+
+        suffix_last = 'l',    -- Suffix to search with "prev" method
+        suffix_next = 'n',    -- Suffix to search with "next" method
+      },
+
+      -- Number of lines within which surrounding is searched
+      n_lines = 20,
+
+      -- Whether to respect selection type:
+      -- - Place surroundings on separate lines in linewise mode.
+      -- - Place surroundings on each line in blockwise mode.
+      respect_selection_type = false,
+
+      -- How to search for surrounding (first inside current line, then inside
+      -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+      -- 'cover_or_nearest', 'next', 'prev', 'nearest'. For more details,
+      -- see `:h MiniSurround.config`.
+      search_method = 'cover',
+
+      -- Whether to disable showing non-error feedback
+      -- This also affects (purely informational) helper messages shown after
+      -- idle time if user input is required.
+      silent = false,
+    }
   end
 }
