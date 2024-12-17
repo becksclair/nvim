f1c65bd55fd061938c5f5877969b66f363263282
Rebecca Clair <rebecca@heliasar.com>
2024-12-17 16:53:53 +0100

    chore: aerial setup tweaks

--- lua/becks/plugins/aerial.lua
+++ lua/becks/plugins/aerial.lua
@@ -5,17 +5,17 @@ return {
     backends = { "treesitter", "lsp", "markdown", "asciidoc", "man" },
     -- optionally use on_attach to set keymaps when aerial has attached to a buffer
     on_attach = function(bufnr)
-        -- Jump forwards/backwards with '{' and '}'
-        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-      end,
+      -- Jump forwards/backwards with '{' and '}'
+      vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+      vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+    end,
   },
   -- Optional dependencies
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons"
   },
-  config = function()
+  init = function()
     vim.keymap.set("n", "<leader>i", "<cmd>AerialToggle<CR>")
   end
 }
