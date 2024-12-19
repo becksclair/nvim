e48ae60cc1c93adcb4e87d188ace46c27223add6
Rebecca Clair <rebecca@heliasar.com>
2024-12-19 04:42:15 +0100

    feat: refactor linter configuration

--- lua/becks/plugins/nvim-lint.lua
+++ lua/becks/plugins/nvim-lint.lua
@@ -2,4 +2,45 @@ return {
   'mfussenegger/nvim-lint',
   lazy = true,
   event = "BufReadPost",
+  opts = {
+    linters = {
+      shellcheck = {
+        args = {
+          '-s',
+          'bash',
+          '--format',
+          'json',
+          '-'
+        }
+      }
+    },
+
+    linters_by_ft = {
+      -- markdown = {'vale',},
+      -- python = { 'flake8', 'pylint', },
+      python = { 'ruff' },
+      sql = { 'sqlfluff', },
+
+      bash = { 'shellcheck', },
+      shell = { 'shellcheck', },
+      zsh = { 'shellcheck', },
+      cpp = { 'clangtidy' },
+
+      -- yaml = { 'cfn_lint' },
+
+      -- vlang = { 'vlang' },
+
+      -- rust = { 'clippy' }
+    }
+  },
+
+  config = function(_, opts)
+    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+      callback = function()
+        require("lint").try_lint()
+      end,
+    })
+
+    vim.keymap.set("n", "<leader>bl", require("lint").try_lint, { desc = "Lint file" })
+  end,
 }
