eeaf0c79ae8bf6d65b00788971063fd96c7c59e3
Rebecca Clair <rebecca@heliasar.com>
2024-12-19 04:35:33 +0100

    chore: cleanup

--- after/plugin/neogit.lua
+++
@@ -1,6 +0,0 @@
-vim.keymap.set("n", "<leader>gs", ":Neogit kind=floating<CR>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>gc", ":Neogit commit<CR>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>gp", ":Neogit pull<CR>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>gP", ":Neogit push<CR>", { silent = true, noremap = true })
--- vim.keymap.set("n", "<leader>gb", ":Telescope git_branches<CR>", { silent = true, noremap = true })
--- vim.keymap.set("n", "<leader>gB", ":G blame<CR>", { silent = true, noremap = true })
