bd65afa41100903a4848eb26088990fbe21ca7e9
Rebecca Clair <rebecca@heliasar.com>
2026-04-16 04:38:53 +0200

    feat(nvim-colorizer): migrate to catgoose fork with new config structure

--- lua/becks/plugins/nvim-colorizer.lua
+++ lua/becks/plugins/nvim-colorizer.lua
@@ -1,37 +1,53 @@
 return {
-  'NvChad/nvim-colorizer.lua',
+  'catgoose/nvim-colorizer.lua',
   event = "BufEnter",
   opts = {
     filetypes = { "*" },
-    user_default_options = {
-      names = true, -- "Name" codes like Blue or blue
-      RGB = true, -- #RGB hex codes
-      RRGGBB = true, -- #RRGGBB hex codes
-      RRGGBBAA = false, -- #RRGGBBAA hex codes
-      AARRGGBB = false, -- 0xAARRGGBB hex codes
-      rgb_fn = true, -- CSS rgb() and rgba() functions
-      hsl_fn = true, -- CSS hsl() and hsla() functions
-      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-      -- Highlighting mode.  'background'|'foreground'|'virtualtext'
-      mode = "background", -- Set the display mode
-      -- Tailwind colors.  boolean|'normal'|'lsp'|'both'.  True is same as normal
-      tailwind = true, -- Enable tailwind colors
-      -- parsers can contain values used in |user_default_options|
-      sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
-      -- Virtualtext character to use
-      virtualtext = "■",
-      -- Display virtualtext inline with color
-      virtualtext_inline = true,
-      -- Virtualtext highlight mode: 'background'|'foreground'
-      virtualtext_mode = "foreground",
-      -- update color values even if buffer is not focused
-      -- example use: cmp_menu, cmp_docs
+    buftypes = {},
+    user_commands = true,
+    options = {
+      parsers = {
+        -- Enable named colors (Blue, red, etc.)
+        names = {
+          enable = true,
+          lowercase = true,
+          camelcase = true,
+        },
+        -- Hex color formats (#RGB, #RRGGBB, etc.)
+        hex = {
+          default = true,
+          rgb = true,
+          rgba = true,
+          rrggbb = true,
+          rrggbbaa = false,
+          aarrggbb = false,
+        },
+        -- CSS functions: rgb(), rgba(), hsl(), hsla()
+        rgb = { enable = true },
+        hsl = { enable = true },
+        -- Enable all CSS features preset
+        css = true,
+        css_fn = true,
+        -- Tailwind colors
+        tailwind = {
+          enable = true,
+          lsp = false,
+        },
+        -- Sass variables
+        sass = {
+          enable = true,
+          parsers = { css = true },
+        },
+      },
+      display = {
+        mode = "background",
+        virtualtext = {
+          char = "■",
+          position = "after",
+          hl_mode = "foreground",
+        },
+      },
       always_update = true,
     },
-    -- all the sub-options of filetypes apply to buftypes
-    buftypes = {},
-    -- Boolean | List of usercommands to enable
-    user_commands = true, -- Enable all or some usercommands
   },
 }
